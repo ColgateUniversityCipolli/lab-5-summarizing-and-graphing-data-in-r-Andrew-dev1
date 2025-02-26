502458534d401e14c05198e9ccd931c058df5977
Andrew-dev1 <andrli20140@gmail.com>
2025-02-26 12:09:29 -0500

    took out step 1 work

--- ali-lab5.R
+++ ali-lab5.R
@@ -1,27 +1,11 @@
 library(tidyverse)
 library(xtable)
 
+# reading data
 training.csv <- read_csv("data/essentia.data.csv") 
 allentown <- read_csv("data/essentia.data.allentown.csv")
 
 # ctrl, shift, c comments many lines 
-# variable <- allentown$overall_loudness
-# training.csv %>%
-#   group_by(artist) %>%
-#   summarise(minimum = min(overall_loudness), 
-#             LF = quantile(overall_loudness, 0.25) -1.5*(IQR(overall_loudness)),
-#             RF = quantile(overall_loudness, 0.25) +1.5*IQR(overall_loudness),
-#             maximum = max(overall_loudness)) %>%
-#   # create two new rows of range distances
-#   mutate(out.of.range = if_else( variable < minimum | 
-#                                    variable > maximum, TRUE, FALSE)) %>%
-#   mutate(unusual = if_else( variable < LF | 
-#                               variable > RF, TRUE, FALSE)) %>%
-#   rowwise() %>% #apply row by row
-#   mutate(description = if (out.of.range) {"Out of Range"}
-#          else if(unusual){"Unusual"}
-#          else{"Within Range"}
-#   )
 
 #step 2 function
 analysis <- function(feature){
